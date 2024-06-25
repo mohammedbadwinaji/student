f0d1a51975be88fd7e4fd266229f9f025ca0af94
Mohammed Naji <mna816008@gmail.com>
2024-06-25 23:45:07 +0300

    any

--- src/pages/Teach.tsx
+++ src/pages/Teach.tsx
@@ -2,7 +2,7 @@ export default function Teach() {
   return (
     <div>
         Teach on Our Website  
-        <a href="localhost:7000" target="_blank"> go to Instructor Dashoard </a>    
+        <a href="https://mohammedbadwinaji.github.io/instructor/" target="_blank"> go to Instructor Dashoard </a>    
     </div>
   )
 }
